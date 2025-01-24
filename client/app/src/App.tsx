dcf507c0416ea36648b6dbed1e3ba8d7f5351d91
= <=>
2025-01-24 20:41:24 +0530

    Initial commit

---
+++ client/app/src/App.tsx
@@ -0,0 +1,23 @@
+import { ConnectButton } from "thirdweb/react";
+import thirdwebIcon from "./thirdweb.svg";
+import { client } from "./client";
+
+export function App() {
+	return (
+		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+			<div className="py-20">
+
+				<div className="flex justify-center mb-20">
+					<ConnectButton
+						client={client}
+						appMetadata={{
+							name: "Example app",
+							url: "https://example.com",
+						}}
+					/>
+				</div>
+
+			</div>
+		</main>
+	);
+}
